3e3932609f249fc6020d23bf70cb49069b179ea9
Edwin Lin <edwinlin@gmail.com>
2019-01-09 21:01:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,13 @@
 # code here!
-class School(name)
+class School
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  attr_accessor :school_name, :roster
+  
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+
 end
